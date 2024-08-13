b8b96f70ca25a603b29e0e8b8e726689d9d3a387
sriram <sriram4650@gmail.com>
2024-08-13 15:59:54 +0530

    ingnition of app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div",{id :"parent"},
   [React.createElement("div",{id :"child"},
     [React.createElement("h1",{}, "i am an h1 element"),React.createElement("h2",{}, "i am an h2 element")]),React.createElement("div",{id :"child"},
