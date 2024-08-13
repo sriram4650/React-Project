3e42d845d8d038f91bb027351489f7790efb2338
sriram <sriram4650@gmail.com>
2024-08-13 13:13:19 +0530

    initialised react

---
+++ App.js
@@ -0,0 +1,6 @@
+const parent = React.createElement("div",{id :"parent"},
+  [React.createElement("div",{id :"child"},
+    [React.createElement("h1",{}, "i am an h1 element"),React.createElement("h2",{}, "i am an h2 element")]),React.createElement("div",{id :"child"},
+      [React.createElement("h1",{}, "i am an h1 element"),React.createElement("h2",{}, "i am an h2 element")])]); 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
